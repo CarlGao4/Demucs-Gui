834dc7253875fa195fe639d38c7999a33b1b84bb
CarlGao4 <gaoweiqicarl1@163.com>
2022-03-31 09:47:07 +0800

    Update RWCore.py

--- GUI/RWCore.py
+++ GUI/RWCore.py
@@ -1,9 +1,25 @@
+# Demucs-GUI 0.1a1
+# Copyright (C) 2022  Carl Gao, Jize Guo, Rosario S.E.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 import julius
 import torch
 import torchaudio
 import os
-import time
+import _thread
 from DemucsCallCore import *
 
 def convert_audio(wav, from_samplerate, to_samplerate, channels):
@@ -50,37 +66,43 @@ def process(model: HDemucs,
             infile: pathlib.Path,
             write: bool = True,
             outpath: pathlib.Path = pathlib.Path(""),
-            duration: float = 10.,
+            split: float = 10.,
             overlap: float = 0.25,
             sample_rate: int = 44100,
             shifts: int = 1,
-            device: Literal["cpu", "cuda"] = "cuda" if torch.cuda.is_available() else "cpu"):
-    duration = int(duration * sample_rate)
-    overlap = int(overlap * duration)
+            device: Literal["cpu", "cuda"] = "cuda" if torch.cuda.is_available() else "cpu",
+            callback = None):
+    def call(status):
+        if callable(callback):
+            _thread.start_new_thread(callback, (status, ))
+    split = int(split * sample_rate)
+    overlap = int(overlap * split)
+    call('Loading file')
     audio = load_audio(str(infile), sample_rate)
     orig_len = audio.shape[0]
-    n = int(np.ceil((orig_len - overlap) / (duration - overlap)))
-    audio = np.pad(audio, [(0, n * (duration - overlap) + overlap - orig_len), (0, 0)])
+    n = int(np.ceil((orig_len - overlap) / (split - overlap)))
+    audio = np.pad(audio, [(0, n * (split - overlap) + overlap - orig_len), (0, 0)])
+    call('Loading model to device %s' % device)
+    model.to(device)
     stems = GetData(model)["sources"]
     new_audio = np.zeros((len(stems), 2, audio.shape[0]))
     total = np.zeros(audio.shape[0])
-    print("Total splits of '%s' : %d" % (str(infile), n))
-    print(time.time())
+    call("Total splits of '%s' : %d" % (str(infile), n))
     for i in range(n):
-        print("Processing splited", i+1)
-        l = i * (duration - overlap)
-        r = l + duration
+        call("Separation %d/%d" % (i+1, n))
+        l = i * (split - overlap)
+        r = l + split
         result = Apply(model, torch.from_numpy(audio[l:r].transpose()).to(device), shifts=shifts)
         for (i, stem) in enumerate(stems):
             new_audio[i, :, l:r] += result[stem].cpu().numpy()
         total[l:r] += 1
-    print(time.time())
     if write:
+        call("Writing to file")
         outpath.mkdir(exist_ok=True)
         os.chdir(outpath)
         for i in range(len(stems)):
             stem = (new_audio[i] / total)[:orig_len]
-            #print((new_audio[i] / total)[:orig_len].shape, torch.from_numpy((new_audio[i] / total)[:orig_len]).shape)
-            torchaudio.save(f'{stems[i]}.wav', torch.from_numpy(stem), sample_rate)
+            torchaudio.save(f'{stems[i]}.wav', i16_pcm(torch.from_numpy(stem)), sample_rate)
     else:
         pass
+
