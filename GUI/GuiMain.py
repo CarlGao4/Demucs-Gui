8ff1cf093092f0763dc15653d3e0497eb33f25b4
CarlGao4 <gaoweiqicarl1@163.com>
2022-04-01 19:14:36 +0800

    Update GuiMain.py

--- GUI/GuiMain.py
+++ GUI/GuiMain.py
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>."""
 
-__version__ = "0.1a1"
+__version__ = "0.1a2"
 
 import time
 
